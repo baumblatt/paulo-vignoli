9230fedf4b5446df053ae5d8c3feb472f96880c3
Bernardo Baumblatt <baumblatt@gmail.com>
2018-10-08 17:11:59 -0300

    scaffolding do store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreModule} from '@ngrx/store';
+import {reducers, metaReducers} from './reducers';
 
 @NgModule({
     declarations: [
@@ -15,7 +15,7 @@ import { reducers, metaReducers } from './reducers';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        StoreModule.forRoot(reducers, { metaReducers })
+        StoreModule.forRoot(reducers, {metaReducers})
     ],
     providers: [],
     bootstrap: [AppComponent]
