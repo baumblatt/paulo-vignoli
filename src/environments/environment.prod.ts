06594d67e3295a520de1431009800fe08350cc0b
Bernardo Baumblatt <baumblatt@gmail.com>
2018-10-10 13:04:34 -0300

    configuração do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyDZynm_kddGYcufKp-NE9P8W-UkMHwioGM',
+        authDomain: 'paulo-vignoli.firebaseapp.com',
+        databaseURL: 'https://paulo-vignoli.firebaseio.com',
+        projectId: 'paulo-vignoli',
+        storageBucket: 'paulo-vignoli.appspot.com',
+        messagingSenderId: '73307764164',
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyDZynm_kddGYcufKp-NE9P8W-UkMHwioGM',
+        authDomain: 'paulo-vignoli.firebaseapp.com',
+        databaseURL: 'https://paulo-vignoli.firebaseio.com',
+        projectId: 'paulo-vignoli',
+        storageBucket: 'paulo-vignoli.appspot.com',
+        messagingSenderId: '73307764164',
+    }
 };
 
 /*
