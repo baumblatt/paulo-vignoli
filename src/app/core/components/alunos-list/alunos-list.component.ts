ba96a4f7a576b83a1dbd74f0dffe7ddd6a08edd4
Bernardo Baumblatt <baumblatt@gmail.com>
2018-10-10 13:54:48 -0300

    apresentação da lista de alunos.

--- src/app/core/components/alunos-list/alunos-list.component.ts
+++ src/app/core/components/alunos-list/alunos-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Aluno} from '../../store/models/aluno.model';
 
 @Component({
     selector: 'app-alunos-list',
@@ -6,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./alunos-list.component.scss']
 })
 export class AlunosListComponent implements OnInit {
+
+    @Input()
+    alunos: Aluno[];
 
     constructor() {
     }
--- src/app/core/containers/alunos/alunos.component.ts
+++ src/app/core/containers/alunos/alunos.component.ts
@@ -1,19 +1,27 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {Aluno} from '../../store/models/aluno.model';
+import {CoreState} from '../../store/reducers/global.reducer';
+import {getAlunos} from '../../store/selectors/alunos.selectors';
 
 @Component({
-  selector: 'app-alunos',
-  templateUrl: './alunos.component.html',
-  styleUrls: ['./alunos.component.scss']
+    selector: 'app-alunos',
+    templateUrl: './alunos.component.html',
+    styleUrls: ['./alunos.component.scss']
 })
 export class AlunosComponent implements OnInit {
 
-  pesquisar = new FormControl();
+    pesquisar = new FormControl();
 
-  constructor() {
-  }
+    alunos$: Observable<Aluno[]>;
 
-  ngOnInit() {
-  }
+    constructor(private store: Store<CoreState>) {
+    }
+
+    ngOnInit() {
+        this.alunos$ = this.store.pipe(select(getAlunos));
+    }
 
 }
