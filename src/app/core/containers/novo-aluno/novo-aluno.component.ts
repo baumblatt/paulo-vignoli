94b458af09f36687ffb6cb86007d62b58141c492
Bernardo Baumblatt <baumblatt@gmail.com>
2018-10-10 08:19:34 -0300

    mais campos no formulário de novo aluno

--- src/app/core/containers/novo-aluno/novo-aluno.component.ts
+++ src/app/core/containers/novo-aluno/novo-aluno.component.ts
@@ -2,16 +2,19 @@ import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-novo-aluno',
-  templateUrl: './novo-aluno.component.html',
-  styleUrls: ['./novo-aluno.component.scss']
+    selector: 'app-novo-aluno',
+    templateUrl: './novo-aluno.component.html',
+    styleUrls: ['./novo-aluno.component.scss']
 })
 export class NovoAlunoComponent {
-  alunoForm = this.fb.group({
-    nome: [null, Validators.required],
-    responsavel: null
-  });
+    alunoForm = this.fb.group({
+        nome: [null, Validators.required],
+        responsavel: null,
+        telefone: null,
+        email: [null, Validators.email],
+        nascimento: null
+    });
 
-  constructor(private fb: FormBuilder) {
-  }
+    constructor(private fb: FormBuilder) {
+    }
 }
