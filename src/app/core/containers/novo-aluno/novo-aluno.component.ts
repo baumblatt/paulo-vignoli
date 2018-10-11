e22f0237d871003d13f7b6b0a400f8eaf4e990f4
Bernardo Baumblatt <baumblatt@gmail.com>
2018-10-11 18:44:00 -0300

    upload de avatar para aluno.

--- src/app/core/containers/novo-aluno/novo-aluno.component.ts
+++ src/app/core/containers/novo-aluno/novo-aluno.component.ts
@@ -1,16 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
-import {Store} from '@ngrx/store';
-import {AlunosAction} from '../../models/action.model';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {AlunosAction, AvatarActions} from '../../models/action.model';
+import {AvatarState} from '../../store/reducers/avatar.reducer';
 import {CoreState} from '../../store/reducers/global.reducer';
+import {getAvatarState} from '../../store/selectors/avatar.selectors';
 
 @Component({
     selector: 'app-novo-aluno',
     templateUrl: './novo-aluno.component.html',
     styleUrls: ['./novo-aluno.component.scss']
 })
-export class NovoAlunoComponent {
+export class NovoAlunoComponent implements OnInit {
+
+    avatar$: Observable<AvatarState>;
+
     alunoForm = this.fb.group({
+        avatar: null,
         nome: [null, Validators.required],
         responsavel: null,
         telefone: null,
@@ -21,7 +28,15 @@ export class NovoAlunoComponent {
     constructor(private store: Store<CoreState>, private fb: FormBuilder) {
     }
 
+    ngOnInit(): void {
+        this.avatar$ = this.store.pipe(select(getAvatarState));
+    }
+
     inserir() {
         this.store.dispatch({type: AlunosAction.INSERIR, payload: this.alunoForm.getRawValue()});
+    }
+
+    avatar(event) {
+        this.store.dispatch({type: AvatarActions.UPLOAD, payload: event.target.files[0]});
     }
 }
